0db2a34572fa396b0a26eb1b059680134df1480e
Kirill Svetlov <kirill.svetlov123@gmail.com>
2025-02-02 16:44:33 +0300

    Complete task 13

---
+++ Homework/Task 13/calculator.js
@@ -0,0 +1,29 @@
+function simpleCalculate(num1, num2, calculateFunction) {
+    return calculateFunction(num1, num2)
+}
+
+const additionNumbers = function (num1, num2) {
+    return num1 + num2
+}
+
+const substractionNumbers = function (num1, num2) {
+    return num1 - num2
+}
+
+const multiplyNumbers = function (num1, num2) {
+    return num1 * num2
+}
+
+const divisionNumbers = function (num1, num2) {
+    return num1 / num2
+}
+
+const operations = {
+    addition: additionNumbers,
+    substraction: substractionNumbers,
+    multiply: multiplyNumbers,
+    division: divisionNumbers,
+}
+  
+const selectedOperation = "multiply";
+console.log(simpleCalculate(6, 3, operations[selectedOperation]));
